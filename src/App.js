c25c9ab1290c175bb93f5da8678b32a5eceb8313
katia santos <katiamsa@gmail.com>
2022-09-14 21:58:16 -0400

    created folder structure and develop branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chefsPage" element={<ChefsPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/logIn" element={<LogInPage />} />
+        <Route path="/chefs/chef/:id" element={<ChefsDetailsPage />} />
+        <Route path="/booking" element={<BookingFormPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
